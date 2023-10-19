515f7f527ec0e374688e914b0e74d314892da4a6
Felipe Santos <79290604+FelipeSantw@users.noreply.github.com>
2023-10-19 02:41:12 -0300

    Update Tratando_dados_planilha.py

--- Tratando_dados_planilha.py
+++ Tratando_dados_planilha.py
@@ -1,3 +1,20 @@
+"""Separar string a cada 2 caracteres
+
+Original file is located at
+    https://colab.research.google.com/drive/17KJJqsPOsxPfIzjrrFtdK28NVCEA7Wet
+
+# **Trabalhando com dados**
+
+**Linguagem: Python;
+Plataforma utilizada: Google Colab;**
+
+Objetivo:
+
+- Export: Coletar dados de uma planilha excel contendo uma série de endereços MAC;
+- Tranform: aplicar formatação da coluna "MAC", realizando separação dos dados a cada 2 caracteres, inserindo o valor ":". Padronizando estes endereços MAC;
+- Carregar essa planilha atualizada em um computador local.
+"""
+
 import pandas as pd
 from google.colab import files
 
